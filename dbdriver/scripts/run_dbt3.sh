a3955600c2c17d12235a4a437e2c0705dfdb09e7
jztpcw <devnull@localhost>
2002-11-25 21:41:04 +0000

    [project @ 403] *** empty log message ***

--- dbdriver/scripts/run_dbt3.sh
+++ dbdriver/scripts/run_dbt3.sh
@@ -1,58 +1,63 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]; then
-        echo "Usage: ./run_dbt3.sh <scale_factor>"
+if [ $# -lt 3 ]; then
+        echo "Usage: ./run_dbt3.sh <scale_factor> <num_stream> <dbt3_dir> [seed]"
         exit
 fi
 
 scale_factor=$1
-dbt3_dir="/home/jenny/src/dbt3"
+num_stream=$2
+dbt3_dir=$3
 run_dir="$dbt3_dir/run"
-
-run_id_file="$run_dir/run_id"
-echo "run_id_file $run_id_file"
-
-if [ ! -f $run_id_file ];
-then
-	echo "creating file $run_id_file"
-	echo "0" > ${run_id_file}
-fi
-
-run_id=`cat $run_id_file`
-let "run_id=$run_id+1"
-echo $run_id>$run_id_file
-
-output_dir="$run_dir/output/$run_id"
-if [ ! -d $output_dir ];
-then 
-	echo "mkdir $output_dir"
-	mkdir $output_dir
-fi
-
-script_log_file="$output_dir/script_out.log"
 seed_file="$run_dir/seed"
 
-echo "start dbt3 run: $run_id">$script_log_file
-echo >> $script_log_file
+if [ $# -eq 4 ]; then
+	echo "running dbt3 with seed $4"
+	echo "$4" > $seed_file
+else
+	echo "running dbt3 with default seed"
+	echo "`date`: generate seed0" 
+	$dbt3_dir/dbdriver/scripts/init_seed.sh > $seed_file
+fi
 
-echo "start load test `date`" >> $script_log_file
+GTIME="${dbt3_dir}/dbdriver/utils/gtime"
+
+#clean time_statistic table
+echo "sql_execute delete from time_statistic"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute delete from time_statistic"
+
+#get time stamp
+s_time_dbt3=`$GTIME`
+echo "sql_execute insert into time_statistic (task_name, s_time) values ('DBT3', timestamp)"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('DBT3', timestamp)"
+
+#***load test
+echo "`date`: start load test" 
 #get the start time
-s_time=`date +%H%M%s`
-#$dbt3_dir/scripts/sapdb/build_db.sh > $output_dir/build_db.log
-e_time=`date +%H%M%s`
-echo "load test end `date`" >> $script_log_file
-let "load_time=$e_time-$s_time"
-echo "elapsed time for load test $load_time" >> $script_log_file
-echo >> $script_log_file
+s_time=`$GTIME`
+echo "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
+#$dbt3_dir/scripts/build_db/build_db.sh > build_db.log
+sleep 3
+echo "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
+e_time=`$GTIME`
+echo "`date`: load test end" 
+let "diff_time=$e_time-$s_time"
+echo "elapsed time for load test $diff_time" 
 
-echo "`date` generate seed0" >> $script_log_file
-$dbt3_dir/dbdriver/scripts/init_seed.sh > $seed_file
+cd $dbt3_dir/dbdriver/scripts
+i=1
+while [ $i -le 2 ]
+do
+        echo "start performance test $i"
+	./run_perf_test.sh $scale_factor $i $dbt3_dir $num_stream
+        let "i=$i+1"
+done
 
-./run_perf_test.sh 1 1 $run_id $dbt3_dir
-#echo "start performance test run 1 `date`">> $script_log_file
-#run_perf_test.sh $scale_factor 1 $run_id
-#echo "end performance test run 1 `date`">> $script_log_file
-#echo >> $script_log_file
-#echo "start performance test run 2 `date`">> $script_log_file
-#run_perf_test.sh $scale_factor 2 $run_id
-#echo "end performance test run 2 `date`">> $script_log_file
+echo "sql_execute update time_statistic set e_time=timestamp where task_name='DBT3'"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='DBT3'"
+e_time_dbt3=`$GTIME`
+echo "`date`: dbt3 test end" 
+let "diff_time=$e_time_dbt3-$s_time_dbt3"
+echo "elapsed time for dbt3 test $diff_time" 
--- dbdriver/scripts/run_perf_test.sh
+++ dbdriver/scripts/run_perf_test.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
 
 if [ $# -ne 4 ]; then
-        echo "Usage: ./run_perf_test.sh <scale_factor> <perf_run_number> <run_id> <dbt3_dir>"
+        echo "Usage: ./run_perf_test.sh <scale_factor> <perf_run_number> <dbt3_dir> <num_stream>"
         exit
 fi
 
 scale_factor=$1
-perf_run_number=$2
-run_id=$3
-dbt3_dir=$4
+perf_run_num=$2
+dbt3_dir=$3
+num_stream=$4
+GTIME="${dbt3_dir}/dbdriver/utils/gtime"
 
-qgen_dir="$dbt3_dir/datagen/dbgen"
-run_dir="$dbt3_dir/run"
-seed_file="$run_dir/seed"
-output_dir="$run_dir/output/$run_id"
-query_file="$run_dir/power_query"
-param_file="$run_dir/power_param"
+echo "`date`:=======performance test $perf_run_num========"
+s_time=`$GTIME`
+echo "sql_execute insert into time_statistic (task_name, s_time) values ('PERF${perf_run_num}', timestamp)"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('PERF${perf_run_num}', timestamp)"
 
-#generate the queries for power test
-cd $qgen_dir
-./qgen -c -r `cat $seed_file` -p 0 -s $scale_factor -l $param_file > $query_file
+#***run power test
+./run_power_test.sh $scale_factor $perf_run_num $dbt3_dir
+
+#***run throughput test
+./run_throughput_test.sh $scale_factor $perf_run_num $dbt3_dir $num_stream
+
+echo "sql_execute update time_statistic set e_time=timestamp where task_name='PERF${perf_run_num}'"
+dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='PERF${perf_run_num}'"
+e_time=`$GTIME`
+echo "`date`: end performance test run ${perf_run_num} "
+let "diff_time=$e_time-$s_time"
+echo "elapsed time for performance test ${perf_run_num} $diff_time"
