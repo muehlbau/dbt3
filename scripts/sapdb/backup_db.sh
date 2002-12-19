abd4f7e242e4085a7b9b838f137aaf63562fc034
jztpcw <devnull@localhost>
2002-12-19 21:50:38 +0000

    [project @ 482] add medium definition

--- scripts/sapdb/backup_db.sh
+++ scripts/sapdb/backup_db.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
-_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d DBT3 -u dbm,dbm 2>&1
+echo "set backup parameters..."
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
+medium_put data /dbt3/datasave FILE DATA 0 8 YES
+medium_put incr /dbt3/incremental FILE PAGES 0 8 YES
+medium_put auto /dbt3/autosave FILE AUTO
 util_connect dbm,dbm
-backup_start data
+backup_start data migration
+backup_start incr migration
 autolog_on
+util_release
 quit
 EOF`
 _test=`echo $_o | grep OK`
