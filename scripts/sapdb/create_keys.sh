1f310f803d114f2feaffe64ea2d0bc2df8e45aa6
jztpcw <devnull@localhost>
2002-12-20 21:03:31 +0000

    [project @ 494] not used anymore

--- scripts/sapdb/create_keys.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-dbmcli -d DBT1 -u dbm,dbm -uSQL dbt,dbt -i create_keys.sql
--- scripts/sapdb/create_keys.sql
+++
@@ -1,1 +0,0 @@
-sql_execute alter table lineitem add foreign key fk_l_partkey ( l_partkey ) references part ( p_partkey )
