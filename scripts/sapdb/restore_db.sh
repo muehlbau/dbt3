e405a1e8228a9f3ccd6956fb4ee7c5c7a5840e0d
jztpcw <devnull@localhost>
2002-12-20 21:37:27 +0000

    [project @ 503] added incremental restore, removed backup at the end

--- scripts/sapdb/restore_db.sh
+++ scripts/sapdb/restore_db.sh
@@ -1,9 +1,11 @@
 echo "recover db"
-_o=`cat <<EOF | dbmcli -d DBT3 -u dbm,dbm 2>&1
+_o=`cat <<EOF | dbmcli -d $SID -u dbm,dbm 2>&1
 db_cold
-util_connect
+util_connect dbm,dbm
 util_execute INIT CONFIG
 recover_start data
+recover_start incr
+util_release
 quit
 EOF`
 _test=`echo $_o | grep OK`
@@ -11,6 +13,3 @@ if [ "$_test" = "" ]; then
         echo "db recover failed: $_o"
         exit 1
 fi
-
-echo "back up database"
-./backup_db.sh
