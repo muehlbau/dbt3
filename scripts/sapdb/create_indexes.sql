6cc53d093e929bc104ce7c886febc9a543a6bf73
jztpcw <devnull@localhost>
2002-12-20 21:21:07 +0000

    [project @ 499] added commit after each index creation

--- scripts/sapdb/create_indexes.sql
+++ scripts/sapdb/create_indexes.sql
@@ -1,14 +1,26 @@
 sql_execute create index i_l_shipdate on lineitem (l_shipdate asc)
+sql_execute commit
 sql_execute create index i_l_suppkey_partkey on lineitem (l_partkey asc, l_suppkey asc)
+sql_execute commit
 sql_execute create index i_l_partkey on lineitem (l_partkey asc)
+sql_execute commit
 sql_execute create index i_l_suppkey on lineitem (l_suppkey asc)
+sql_execute commit
 sql_execute create index i_l_receiptdate on lineitem (l_receiptdate asc)
+sql_execute commit
 sql_execute create index i_l_orderkey on lineitem (l_orderkey asc)
+sql_execute commit
 sql_execute create index i_c_nationkey on customer (c_nationkey asc)
+sql_execute commit
 sql_execute create index i_o_orderdate on orders (o_orderdate asc)
+sql_execute commit
 sql_execute create index i_o_custkey on orders (o_custkey asc)
+sql_execute commit
 sql_execute create index i_s_nationkey on supplier (s_nationkey asc)
+sql_execute commit
 sql_execute create index i_ps_partkey on partsupp (ps_partkey asc)
+sql_execute commit
 sql_execute create index i_ps_suppkey on partsupp (ps_suppkey asc)
+sql_execute commit
 sql_execute create index i_n_regionkey on nation (n_regionkey asc)
-
+sql_execute commit
