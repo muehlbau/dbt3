40b8f77524edba53b11fff47ec579b163aee21b0
jztpcw <devnull@localhost>
2002-12-24 19:23:56 +0000

    [project @ 515] use environment variable DBT3_INSTALL_PATH instead of dbt_dir,  used environment variable SID instead of hardcoding dbt3, changed table name time_statistic to time_statistics

--- dbdriver/scripts/run_dbt3.sh
+++ dbdriver/scripts/run_dbt3.sh
@@ -1,62 +1,61 @@
 #!/bin/sh
 
-if [ $# -lt 3 ]; then
-        echo "Usage: ./run_dbt3.sh <scale_factor> <num_stream> <dbt3_dir> [seed]"
+if [ $# -lt 2 ]; then
+        echo "Usage: ./run_dbt3.sh <scale_factor> <num_stream> [seed]"
         exit
 fi
 
 scale_factor=$1
 num_stream=$2
-dbt3_dir=$3
-run_dir="$dbt3_dir/run"
+run_dir="$DBT3_INSTALL_PATH/run"
 seed_file="$run_dir/seed"
 
-if [ $# -eq 4 ]; then
-	echo "running dbt3 with seed $4"
-	echo "$4" > $seed_file
+if [ $# -eq 3 ]; then
+	echo "running $SID with seed $3"
+	echo "$3" > $seed_file
 else
-	echo "running dbt3 with default seed"
+	echo "running $SID with default seed"
 	echo "`date`: generate seed0" 
-	$dbt3_dir/dbdriver/scripts/init_seed.sh > $seed_file
+	$DBT3_INSTALL_PATH/dbdriver/scripts/init_seed.sh > $seed_file
 fi
 
-GTIME="${dbt3_dir}/dbdriver/utils/gtime"
+GTIME="${DBT3_INSTALL_PATH}/dbdriver/utils/gtime"
 
-#clean time_statistic table
-echo "sql_execute delete from time_statistic"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute delete from time_statistic"
+#clean time_statistics table
+echo "sql_execute delete from time_statistics"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute delete from time_statistics"
 
 #get time stamp
 s_time_dbt3=`$GTIME`
-echo "sql_execute insert into time_statistic (task_name, s_time) values ('DBT3', timestamp)"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('DBT3', timestamp)"
+echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('DBT3', timestamp, $s_time_dbt3)"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('DBT3', timestamp, $s_time_dbt3)"
 
 #***load test
 echo "`date`: start load test" 
 #get the start time
 s_time=`$GTIME`
-echo "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
-#$dbt3_dir/scripts/build_db/build_db.sh > build_db.log
+echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
+#$DBT3_INSTALL_PATH/scripts/build_db/build_db.sh > build_db.log
 sleep 3
-echo "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
+echo "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
 e_time=`$GTIME`
 echo "`date`: load test end" 
 let "diff_time=$e_time-$s_time"
 echo "elapsed time for load test $diff_time" 
 
-cd $dbt3_dir/dbdriver/scripts
+cd $DBT3_INSTALL_PATH/dbdriver/scripts
 i=1
 while [ $i -le 2 ]
 do
         echo "start performance test $i"
-	./run_perf_test.sh $scale_factor $i $dbt3_dir $num_stream
+	./run_perf_test.sh $scale_factor $i $num_stream
         let "i=$i+1"
 done
 
-echo "sql_execute update time_statistic set e_time=timestamp where task_name='DBT3'"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='DBT3'"
+echo "sql_execute update time_statistics set e_time=timestamp where task_name='DBT3' and int_time=$s_time_dbt3"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='DBT3' and int_time=$s_time_dbt3"
 e_time_dbt3=`$GTIME`
 echo "`date`: dbt3 test end" 
 let "diff_time=$e_time_dbt3-$s_time_dbt3"
