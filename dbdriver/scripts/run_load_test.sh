61381b1afbf059b1f1ab455bd7205e1ca8c11114
jztpcw <devnull@localhost>
2002-12-24 18:51:37 +0000

    [project @ 513] use environment variable DBT3_INSTALL_PATH instead of dbt_dir,  used environment variable SID instead of hardcoding dbt3, changed table name time_statistic to time_statistics, not tested yet

--- dbdriver/scripts/run_load_test.sh
+++ dbdriver/scripts/run_load_test.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-if [ $# -lt 2 ]; then
-        echo "Usage: ./run_load_test.sh <scale_factor> <dbt3_dir>"
+if [ $# -lt 1 ]; then
+        echo "Usage: ./run_load_test.sh <scale_factor>"
         exit
 fi
 
 scale_factor=$1
-dbt3_dir=$2
 
-GTIME="${dbt3_dir}/dbdriver/utils/gtime"
+GTIME="${DBT3_INSTALL_PATH}/dbdriver/utils/gtime"
 
 #***load test
 echo "`date`: start load test" 
 #get the start time
 s_time=`$GTIME`
-echo "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('LOAD', timestamp)"
-$dbt3_dir/scripts/sapdb/build_db.sh > build_db.log
-echo "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='LOAD'"
+echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
+$DBT3_INSTALL_PATH/scripts/sapdb/build_db.sh > build_db.log
+echo "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
 e_time=`$GTIME`
 echo "`date`: load test end" 
 let "diff_time=$e_time-$s_time"
