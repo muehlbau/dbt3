10a305ff5ba869d2b5e2a2cd771651906cd2be47
jztpcw <devnull@localhost>
2002-12-24 18:37:03 +0000

    [project @ 511] use environment variable DBT3_INSTALL_PATH instead of dbt_dir,  used environment variable SID instead of hardcoding dbt3, changed table name time_statistic to time_statistics, corrected some typo

--- dbdriver/scripts/run_throughput_test.sh
+++ dbdriver/scripts/run_throughput_test.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-if [ $# -ne 4 ]; then
-        echo "Usage: ./run_throughput_test.sh <scale_factor> <perf_run_number> <dbt3_dir> <num_stream>"
+if [ $# -ne 3 ]; then
+        echo "Usage: ./run_throughput_test.sh <scale_factor> <perf_run_number> <num_stream>"
         exit
 fi
 
 scale_factor=$1
 perf_run_number=$2
-dbt3_dir=$3
-num_stream=$4
-GTIME="${dbt3_dir}/dbdriver/utils/gtime"
+num_stream=$3
+qgen_dir="$DBT3_INSTALL_PATH/datagen/dbgen"
+run_dir="$DBT3_INSTALL_PATH/run"
+seed_file="$run_dir/seed"
+
+GTIME="${DBT3_INSTALL_PATH}/dbdriver/utils/gtime"
 
 echo "`date`:=======throughput test $perf_run_number========"
 s_time=`$GTIME`
-echo "sql_execute insert into time_statistic (task_name, s_time) values ('PERF${perf_run_num}.THUPUT', timestamp)"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistic (task_name, s_time) values ('PERF${perf_run_num}.THUPUT', timestamp)"
-
-qgen_dir="$dbt3_dir/datagen/dbgen"
-run_dir="$dbt3_dir/run"
-seed_file="$run_dir/seed"
+echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('PERF${perf_run_number}.THUPUT', timestamp, $s_time)"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('PERF${perf_run_number}.THUPUT', timestamp, $s_time)"
 
 #generate the queries for throughput test
 #and start the streams
@@ -26,23 +25,23 @@ i=1
 while [ $i -le $num_stream ] 
 do
 	echo "start throughput query of stream $i"
-	#./run_throughput_query.sh $scale_factor $perf_run_number $i $dbt3_dir > $run_dir/thuput_qs$i 2>&1 &
+	./run_throughput_query.sh $scale_factor $perf_run_number $i> $run_dir/thuput_qs$i 2>&1 &
 	let "i=$i+1"
 done
 
 #start the refresh stream
-cd $dbt3_dir/dbdriver/scripts
+cd $DBT3_INSTALL_PATH/dbdriver/scripts
 i=1
 while [ $i -le $num_stream ]
 do
         echo "start throughput refresh stream stream $i"
-	./run_refresh_stream.sh $scale_factor $i $perf_run_number $dbt3_dir > ${run_dir}/refresh_stream$i
+	./run_refresh_stream.sh $scale_factor $i $perf_run_number > ${run_dir}/refresh_stream$i 2>&1
         let "i=$i+1"
 done
 
 wait
-echo "sql_execute update time_statistic set e_time=timestamp where task_name='PERF${perf_run_num}.THUPUT'"
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistic set e_time=timestamp where task_name='PERF${perf_run_num}.THUPUT'"
+echo "sql_execute update time_statistics set e_time=timestamp where task_name='PERF${perf_run_number}.THUPUT' and int_time=$s_time"
+dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='PERF${perf_run_number}.THUPUT' and int_time=$s_time"
 e_time=`$GTIME`
 echo "`date`: end throughput test run "
 let "diff_time=$e_time-$s_time"
