e4d53a2dba70d6ffc8986241e5fb4d23f9ee304c
jztpcw <devnull@localhost>
2003-01-02 23:56:06 +0000

    [project @ 542] added reducing DATA_CACHE before restore and execute set_param.sh after restore

--- scripts/sapdb/restore_db.sh
+++ scripts/sapdb/restore_db.sh
@@ -1,4 +1,21 @@
-echo "recover db"
+#!/bin/sh
+
+#note: if DATA_CACHE is too big, it should be reduced before restore
+#otherwise we get I/O error
+echo "changing data_cache to 10000"
+_o=`cat <<EOF |  /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
+param_startsession
+param_put DATA_CACHE 10000
+param_checkall
+param_commitsession
+quit
+EOF`
+_test=`echo $_o | grep ERR`
+if ! [ "$_test" = "" ]; then
+        echo "set parameters failed"
+        exit 1
+fi
+echo "start restoring db"
 _o=`cat <<EOF | dbmcli -d $SID -u dbm,dbm 2>&1
 db_cold
 util_connect dbm,dbm
@@ -13,3 +30,9 @@ if [ "$_test" = "" ]; then
         echo "db recover failed: $_o"
         exit 1
 fi
+
+echo "set database parameters"
+./set_param.sh
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
+db_stop
+db_warm
