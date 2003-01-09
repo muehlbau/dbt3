ec459492b970a4b38e4494d8e0fa6ef2e96f7e58
jztpcw <devnull@localhost>
2003-01-09 17:01:35 +0000

    [project @ 547] added input parameter to determine if we are creating a new db

--- scripts/sapdb/set_param.sh
+++ scripts/sapdb/set_param.sh
@@ -1,22 +1,46 @@
 #!/bin/sh
 
+if [ $# -ne 1 ]; then
+	echo "Usage: ./set_param.sh <0/1>"
+	exit
+fi
+
 # setup database parameters
 echo "set parameters for $SID..."
-_o=`cat <<EOF | dbmcli -d $SID -u dbm,dbm 2>&1
-param_rmfile
-param_startsession
-param_init
-param_put LOG_MODE DEMO
-param_put DATA_CACHE 5000
-param_put MAXUSERTASKS 10
-param_put MAXCPU 2
-param_put _IDXFILE_LIST_SIZE 8192
-param_put _PACKET_SIZE 131072
-param_put DATE_TIME_FORMAT ISO
-param_checkall
-param_commitsession
-quit
-EOF`
+if [ $1 -eq 0 ]; then
+	_o=`cat <<EOF | dbmcli -d $SID -u dbm,dbm 2>&1
+	param_rmfile
+	param_startsession
+	param_init
+	param_put LOG_MODE DEMO
+	param_put DATA_CACHE 262144
+	param_put MAXDATADEVSPACES 10
+	param_put MAXUSERTASKS 80
+	param_put MAXCPU 8
+	param_put _IDXFILE_LIST_SIZE 8192
+	param_put _PACKET_SIZE 131072
+	param_put DATE_TIME_FORMAT ISO
+	param_checkall
+	param_commitsession
+	quit
+	EOF`
+fi
+if [ $1 -eq 1 ]; then
+	_o=`cat <<EOF | dbmcli -d $SID -u dbm,dbm 2>&1
+	param_startsession
+	param_put LOG_MODE DEMO
+	param_put DATA_CACHE 262144
+	param_put MAXDATADEVSPACES 10
+	param_put MAXUSERTASKS 80
+	param_put MAXCPU 8
+	param_put _IDXFILE_LIST_SIZE 8192
+	param_put _PACKET_SIZE 131072
+	param_put DATE_TIME_FORMAT ISO
+	param_checkall
+	param_commitsession
+	quit
+	EOF`
+fi
 _test=`echo $_o | grep OK`
 if [ "$_test" = "" ]; then
         echo "set parameters failed: $_o"
