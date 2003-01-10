358ca84304d9d6d2be122238e0bc80d921ead563
jztpcw <devnull@localhost>
2003-01-10 17:39:27 +0000

    [project @ 559] removed operations on the table time_statistics for load and dbt3 since the table is not created yet. Added getting elapsed time at the end of the script

--- dbdriver/scripts/run_dbt3.sh
+++ dbdriver/scripts/run_dbt3.sh
@@ -21,29 +21,19 @@ fi
 
 GTIME="${DBT3_INSTALL_PATH}/dbdriver/utils/gtime"
 
-#clean time_statistics table
-echo "sql_execute delete from time_statistics"
-dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute delete from time_statistics"
-
 #get time stamp
 s_time_dbt3=`$GTIME`
-echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('DBT3', timestamp, $s_time_dbt3)"
-dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('DBT3', timestamp, $s_time_dbt3)"
 
 #***load test
 echo "`date`: start load test" 
 cd $DBT3_INSTALL_PATH/scripts/sapdb
 #get the start time
 s_time=`$GTIME`
-echo "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
-dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute insert into time_statistics (task_name, s_time, int_time) values ('LOAD', timestamp, $s_time)"
 ./build_db.sh
-echo "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
-dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='LOAD' and int_time=$s_time"
 e_time=`$GTIME`
 echo "`date`: load test end" 
-let "diff_time=$e_time-$s_time"
-echo "elapsed time for load test $diff_time" 
+let "diff_time_load=$e_time-$s_time"
+echo "elapsed time for load test $diff_time_load" 
 
 cd $DBT3_INSTALL_PATH/dbdriver/scripts
 i=1
@@ -54,9 +44,13 @@ do
         let "i=$i+1"
 done
 
-echo "sql_execute update time_statistics set e_time=timestamp where task_name='DBT3' and int_time=$s_time_dbt3"
-dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute update time_statistics set e_time=timestamp where task_name='DBT3' and int_time=$s_time_dbt3"
 e_time_dbt3=`$GTIME`
 echo "`date`: dbt3 test end" 
-let "diff_time=$e_time_dbt3-$s_time_dbt3"
-echo "elapsed time for dbt3 test $diff_time" 
+let "diff_time_dbt3=$e_time_dbt3-$s_time_dbt3"
+echo "elapsed time for dbt3 test $diff_time_dbt3" 
+
+cd $DBT3_INSTALL_PATH/dbdriver/scripts
+echo "execution time"
+./q_time.sh
+echo "elapsed time for load test $diff_time_load"
+echo "elapsed time for dbt3 test $diff_time_dbt3"
