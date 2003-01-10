216dcff1e275280472edb675ffe869212c4684eb
jztpcw <devnull@localhost>
2003-01-10 17:30:10 +0000

    [project @ 557] just change the comment. This query is rewritten by moving some common constraints out the ORs.  Other variant include changing the way revenue is calculated; changing p_container in ... to p_container='' or ..., but I do not see much difference performance wise. So I will use this for now

--- datagen/queries/19.sql
+++ datagen/queries/19.sql
@@ -17,21 +17,21 @@ where
 	(
 		and p_brand = ':1'
 		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
-		and l_quantity >= :4 and l_quantity <= :4 + 10
+		and l_quantity >= :4 and l_quantity <= :4+10
 		and p_size between 1 and 5
 	)
 	or
 	(
 		and p_brand = ':2'
 		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
-		and l_quantity >= :5 and l_quantity <= :5 + 10
+		and l_quantity >= :5 and l_quantity <= :5+10
 		and p_size between 1 and 10
 	)
 	or
 	(
 		and p_brand = ':3'
 		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
-		and l_quantity >= :6 and l_quantity <= :6 + 10
+		and l_quantity >= :6 and l_quantity <= :6+10
 		and p_size between 1 and 15
 	);
 :n -1
