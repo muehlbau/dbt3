9c13c0d372d62f0b9bf1eb0365706065f1c1e36a
jztpcw <devnull@localhost>
2003-01-14 21:18:22 +0000

    [project @ 562] there were extra 'and'

--- datagen/queries/19.sql
+++ datagen/queries/19.sql
@@ -15,23 +15,25 @@ where
 	and l_shipinstruct = 'DELIVER IN PERSON'
 	and
 	(
-		and p_brand = ':1'
+	 (
+		p_brand = ':1'
 		and p_container in ('SM CASE', 'SM BOX', 'SM PACK', 'SM PKG')
 		and l_quantity >= :4 and l_quantity <= :4+10
 		and p_size between 1 and 5
-	)
-	or
-	(
-		and p_brand = ':2'
+	 )
+	 or
+	 (
+		p_brand = ':2'
 		and p_container in ('MED BAG', 'MED BOX', 'MED PKG', 'MED PACK')
 		and l_quantity >= :5 and l_quantity <= :5+10
 		and p_size between 1 and 10
-	)
-	or
-	(
-		and p_brand = ':3'
+	  )
+	  or
+	  (
+		p_brand = ':3'
 		and p_container in ('LG CASE', 'LG BOX', 'LG PACK', 'LG PKG')
 		and l_quantity >= :6 and l_quantity <= :6+10
 		and p_size between 1 and 15
+	  )
 	);
 :n -1
