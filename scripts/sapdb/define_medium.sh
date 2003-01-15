a9a1f6bece98f8c57db0f6907d23a71f9097d13d
jztpcw <devnull@localhost>
2003-01-15 19:18:49 +0000

    [project @ 608] script to define backkup medium

---
+++ scripts/sapdb/define_medium.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+echo "define backup medium"
+_o=`cat <<EOF | /opt/sapdb/depend/bin/dbmcli -d $SID -u dbm,dbm 2>&1
+medium_put data /dbt3/datasave FILE DATA 0 8 YES
+medium_put incr /dbt3/incremental FILE PAGES 0 8 YES
+quit
+EOF`
+_test=`echo $_o | grep OK`
+if [ "$_test" = "" ]; then
+        echo "backup failed: $_o"
+        exit 1
+fi
