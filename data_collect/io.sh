bba267f342220ae67ec526f82612393b3f6b3122
jztpcw <devnull@localhost>
2003-02-17 22:07:14 +0000

    [project @ 746] added code to decide the version of sysstats to call according to kernel version

--- data_collect/io.sh
+++ data_collect/io.sh
@@ -4,5 +4,13 @@
 # I/O data.
 echo "IO Statistics (iostat -d ) " > $3/io.txt
 echo "iostat -d $1 $2">>$3/io.txt
-iostat -d $1 $2 >> $3/io.txt
+VERSION=`uname -r | awk -F "." '{print $2}'`
 
+if [ $VERSION -eq 5 ]
+then
+#use sysstat 4.1.1
+/usr/local/bin/iostat -d $1 $2 >> $3/io.txt
+else
+#use sysstat 4.0.3
+/usr/bin/iostat -d $1 $2 >> $3/io.txt
+fi
