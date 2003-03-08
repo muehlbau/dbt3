a7360d5ce79a2777811241aaffe5de858e52889c
jztpcw <devnull@localhost>
2003-03-08 01:23:39 +0000

    [project @ 791] move *.sql to sapdb

--- data_collect/10.sql
+++
@@ -1,2 +0,0 @@
-sql_execute explain select c_custkey, c_name, sum(l_extendedprice * (1 - l_discount)) as revenue, c_acctbal, n_name, c_address, c_phone, c_comment from customer, orders, lineitem, nation where c_custkey = o_custkey and l_orderkey = o_orderkey and o_orderdate >= '1994-04-01' and o_orderdate < adddate('1994-04-01', 90) and l_returnflag = 'R' and c_nationkey = n_nationkey group by c_custkey, c_name, c_acctbal, c_phone, n_name, c_address, c_comment order by revenue desc
-sql_execute select * from show
