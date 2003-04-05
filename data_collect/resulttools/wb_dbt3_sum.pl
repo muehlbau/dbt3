35e3c84b8b2bd164033876271be464f6922b0142
jztpcw <devnull@localhost>
2003-04-05 00:22:15 +0000

    [project @ 897] changed to create multiple tables

--- data_collect/resulttools/wb_dbt3_sum.pl
+++ data_collect/resulttools/wb_dbt3_sum.pl
@@ -29,179 +29,159 @@ generate dbt3 run summary list
 
 =head1 ARGUMENTS
 
- -inputdir <the result directory>
- -dirname <the directory name glob>
- -filename <the file which contains the numbers>
- -varname <a list of value names we are looking for>
- -outfile <output file name>
  -configfile <config filename to read from> 
- -write <config filename to write to> 
 
 =cut
 
-my ( $inputdir, @dirname, $hlp, @varname, $configfile, $writeme, $filename,
-	$outfile, $average, $std, $fout); 
+my ($configfile, $hlp);
+
+my ($fcf, $cline, @tables, $table_title, $fout, $outfile);
 
 GetOptions(
-	"inputdir=s" => \$inputdir,
-	"dirname=s" => \@dirname,
-	"filename=s" => \$filename,
-	"varname=s" => \@varname,
-	"outfile=s" => \$outfile,
-	"help"      => \$hlp,
 	"configfile=s"    => \$configfile,
-	"write=s"   => \$writeme
+	"help" 		=> \$hlp
 );
 
-my $fcf = new FileHandle;
-my ( $cline, %options );
+if ( !$configfile )
+{
+	die "config file is required";
+}
+
+$fcf = new FileHandle;
 
 if ($hlp) { pod2usage(1); }
 
-if ( $configfile ) {
-	unless ( $fcf->open( "< $configfile" ) ) {
-		die "Missing config file $!";
-	}
-	while ( $cline = $fcf->getline ) {
-		next if ( $cline =~ /^#/ );
-		chomp $cline;
-		my ( $var, $value ) = split /=/, $cline;
-		$options{ $var } = $value;
-		$_ = $var;
-		if ( /varname/ )
-		{
-			$#varname++;
-		}
-		elsif ( /dirname/ )
-		{
-			$#dirname++;
-		}
-	}
-	$fcf->close;
+unless ( $fcf->open( "< $configfile" ) ) {
+	die "Missing config file $!";
 }
-
-if ( $inputdir ) { $options{ 'inputdir' } = $inputdir; }
-elsif ( $options{ 'inputdir' } ) {
-	$inputdir =  $options{ 'inputdir' };
-}
-else
-{
-	die "No inputdir $!";
-}
-
-if ( $filename ) { $options{ 'filename' } = $filename; }
-elsif ( $options{ 'filename' } ) {
-	$filename =  $options{ 'filename' };
-}
-else
-{
-	die "No filename $!";
-}
-
-if ( $outfile ) { $options{ 'outfile' } = $outfile; }
-elsif ( $options{ 'outfile' } ) {
-	$outfile =  $options{ 'outfile' };
-}
-else
-{
-	die "No outfile $!";
-}
-
-for ( my $i=0; $i<=$#varname; $i++ )
-{
-	if ( $varname[$i] )
+# get output file name and tabels to generate
+while ( $cline = $fcf->getline ) {
+	next if ( $cline =~ /^#/ || $cline =~ /^$/ );
+	chomp $cline;
+	my ( $var, $value ) = split /=/, $cline;
+	if ( $var =~ /outfile/ )
 	{
-		$options{ "varname$i" } = $varname[$i];
+		$outfile = $value;
 	}
-	elsif ( $options{ "varname$i" } )
+	elsif ( $var =~ /outtables/ )
 	{
-		$varname[$i] =  $options{ "varname$i" };
-	}
-	else
-	{
-		die "no varname $!";
+		@tables = split /;/, $value;
 	}
 }
-
-for ( my $i=0; $i<=$#dirname; $i++ )
-{
-	if ( $dirname[$i] )
-	{
-		$options{ "dirname$i" } = $dirname[$i];
-	}
-	elsif ( $options{ "dirname$i" } )
-	{
-		$dirname[$i] =  $options{ "dirname$i" };
-	}
-	else
-	{
-		die "no dirname $!";
-	}
-	print "dirname1 $dirname[$i]\n";
-}
-
-if ( $writeme ) {
-	my $ncf = new FileHandle;
-	unless ( $ncf->open( "> $writeme" ) ) { die "can't open file $!"; }
-	my $name;
-	foreach $name ( keys( %options ) ) {
-	    print $ncf $name, "=", $options{ $name }, "\n";
-	}
-	$ncf->close;
-}
+$fcf->close;
 
 $fout = new FileHandle;
 unless ( $fout->open( "> $outfile" ) ) { die "can't open file $!"; }
-print $fout h1("DBT3 run summary"), "\n";;
-print $fout start_table( { -border => undef }), "\n";
-#print the header of the table
-print $fout "<tr><th>Base Kernel</th><th>Patches</th><th>Scale Factor</th><th>Number of Streams</th>";
-for (my $i=0; $i<=$#varname; $i++)
-{
-	print $fout "<th><a href=\"./dbt3_explain.html#$varname[$i]\"> $varname[$i]</a> average</th><th> $varname[$i] Standard Deviation</th>";
-}
+print $fout h1("DBT-3 run summary"), "\n";;
 
-print $fout "<th>Result Page</th></tr>\n";
-
-for (my $j=0; $j<=$#dirname; $j++)
+for (my $s=0; $s<=$#tables; $s++)
 {
-	my ($basekernel, @patches, $num_of_streams, $scale_factor);
-	($scale_factor, $num_of_streams, $basekernel, @patches) = 
-		parse_dir_name($dirname[$j]);
-	print $fout "<tr><td>$basekernel</td>";
-	print $fout "<td><ul>";
-	for (my $i=0; $i<=$#patches; $i++)
+	my ( $inputdir, @dirname, @varname, $filename, $average, $std); 
+
+	my $table_name = $tables[$s];
+	# read from config file
+	unless ( $fcf->open( "< $configfile" ) ) {
+		die "Missing config file $!";
+	}
+
+	# read to this tables config
+	while ( $cline = $fcf->getline ) {
+		chop $cline;
+		last if ( $cline  eq "[$table_name]" );
+	}
+	
+	# read to next config
+	while ( $cline = $fcf->getline ) 
 	{
-		if ( $patches[$i] =~ /plm/ )
+		next if ( $cline =~ /^#/ || $cline =~ /^$/);
+		last if ( $cline =~ /\[table/ );
+		chomp $cline;
+		my ( $var, $value ) = split /=/, $cline;
+		if ( $var =~ /$table_name/ )
 		{
-			$patches[$i] =~ s/plm//;
-			print $fout "<li>
-				<a href=\"http://www.osdl.org/cgi-bin/plm?module=patch_info&patch_id=$patches[$i]\"> PLM $patches[$i]</a>"
+			$table_title = $value;
+		}
+		if ( $var =~ /filename/ )
+		{
+			$filename = $value;
+		}
+		elsif ( $var =~ /dirname/ )
+		{
+			@dirname = split /;/, $value;
+		}
+		elsif ( $var =~ /varname/ )
+		{
+			@varname = split /;/, $value;
+		}
+		elsif ( $var =~ /inputdir/ )
+		{
+			$inputdir = $value;
+		}
+	}
+	
+	print $fout h2("$table_title"), "\n";;
+	print $fout start_table( { -border => undef }), "\n";
+	#print the header of the table
+	print $fout "<tr><th>Base Kernel</th><th>Patches</th><th>Scale Factor</th><th>Number of Streams</th>";
+	for (my $i=0; $i<=$#varname; $i++)
+	{
+		my $tmp_title;
+		# replace the load title
+		if ( $varname[$i] eq 'load') 
+		{ 
+			$tmp_title = "database load (seconds)";
 		}
 		else
 		{
-			print $fout "<li>$patches[$i]"
+			$tmp_title = $varname[$i];;
 		}
+		print $fout "<th><a href=\"./dbt3_explain.html#$varname[$i]\"> $tmp_title</a> average</th><th> $tmp_title Standard Deviation</th>";
 	}
-	print $fout "</ul></td>";
 	
-	print $fout "<td>$scale_factor</td><td>$num_of_streams</td>";
-	for (my $i=0; $i<=$#varname; $i++)
+	print $fout "<th>Result Page</th></tr>\n";
+	
+	for (my $j=0; $j<=$#dirname; $j++)
 	{
-		($average, $std) = cal_average_std( $inputdir, $dirname[$j], $filename, $varname[$i] );
-		printf $fout "<td>%.2f</td><td>%.2f</td>", $average, $std;
+		my ($basekernel, @patches, $num_of_streams, $scale_factor);
+		($scale_factor, $num_of_streams, $basekernel, @patches) = 
+			parse_dir_name($dirname[$j]);
+		print $fout "<tr><td>$basekernel</td>";
+		print $fout "<td><ul>";
+		for (my $i=0; $i<=$#patches; $i++)
+		{
+			if ( $patches[$i] =~ /plm/ )
+			{
+				$patches[$i] =~ s/plm//;
+				print $fout "<li>
+					<a href=\"http://www.osdl.org/cgi-bin/plm?module=patch_info&patch_id=$patches[$i]\"> PLM $patches[$i]</a>"
+			}
+			else
+			{
+				print $fout "<li>$patches[$i]"
+			}
+		}
+		print $fout "</ul></td>";
+		
+		print $fout "<td>$scale_factor</td><td>$num_of_streams</td>";
+		for (my $i=0; $i<=$#varname; $i++)
+		{
+			($average, $std) = cal_average_std( $inputdir, $dirname[$j], $filename, $varname[$i] );
+			printf $fout "<td>%.2f</td><td>%.2f</td>", $average, $std;
+		}
+		#assume the pages are under the same dir
+		my @pagelist = glob("$dirname[$j].html");
+		print $fout "<td><ul>";
+		for (my $k=0; $k<=$#pagelist; $k++)
+		{
+			print $fout "<li><a href=\"$pagelist[$k]\"> $pagelist[$k]</a>"
+		}
+		print $fout "</ul></td></tr>\n";
 	}
-	#assume the pages are under the same dir
-	my @pagelist = glob("$dirname[$j].html");
-	print $fout "<td><ul>";
-	for (my $k=0; $k<=$#pagelist; $k++)
-	{
-		print $fout "<li><a href=\"$pagelist[$k]\"> $pagelist[$k]</a>"
-	}
-	print $fout "</ul></td></tr>\n";
+	print $fout end_table( { -border => undef }), "\n";
+	close($fcf);
 }
-print $fout end_table( { -border => undef }), "\n";
-
+close($fout);
+	
 sub parse_dir_name
 {
 	my ($dirname) = @_;
@@ -236,3 +216,4 @@ sub parse_dir_name
 	print "revalue is ", join(' ', @retvalue), "\n";
 	return @retvalue;
 }
+
