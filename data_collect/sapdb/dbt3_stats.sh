679567166179532c89055cd482d8a2dea9c8bfe1
jztpcw <devnull@localhost>
2003-04-17 23:40:46 +0000

    [project @ 924] do not use  as command parameter when calling sys_stats.sh, added killing sys_stats process at the end of the run

--- data_collect/sapdb/dbt3_stats.sh
+++ data_collect/sapdb/dbt3_stats.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 
 # dbt3_stats.sh: run dbt3 test and collect database and system 
 # statistics
@@ -20,25 +21,9 @@ fi
 scale_factor=$1
 num_stream=$2
 output_dir=$3
-CPUS=`grep -c '^processor' /proc/cpuinfo`
 
-#estimated dbt3 run time
-#dbt3_test_time=21607
-if [ $num_stream -eq 8 ]
-then
-	dbt3_test_time=18700
-elif [ $num_stream -eq 2 ]
-then
-	dbt3_test_time=9770
-elif [ $num_stream -eq 4 ]
-then
-	dbt3_test_time=10640
-elif [ $num_stream -eq 6 ]
-then
-	dbt3_test_time=12177
-else
-	dbt3_test_time=10800
-fi
+#estimated dbt3 run time, make it really long, it will be killed at the end
+dbt3_test_time=21607
 
 duration=0
 interval=0
@@ -62,7 +47,7 @@ dbdriver_script_path=$DBT3_INSTALL_PATH/dbdriver/scripts
 dbdriver_sapdb_path=$DBT3_INSTALL_PATH/dbdriver/scripts/sapdb
 run_path=$DBT3_INSTALL_PATH/run
 seed_file=$DBT3_INSTALL_PATH/run/seed
-sapdb_script_path=$DBT3_INSTALL_PATH/scripts/sapdb
+builddb_path=$DBT3_INSTALL_PATH/builddb
 datacollect_path=$DBT3_INSTALL_PATH/data_collect
 datacollect_sapdb_path=$DBT3_INSTALL_PATH/data_collect/sapdb
 GTIME="${DBT3_INSTALL_PATH}/dbdriver/utils/gtime"
@@ -112,7 +97,7 @@ $datacollect_path/get_config.sh $scale_factor $num_stream $output_dir
 
 #start sys_stats.sh
 echo "start sys_stats.sh"
-$datacollect_path/sys_stats.sh $interval $duration $CPUS $output_dir &
+$datacollect_path/sys_stats.sh $interval $duration $output_dir &
 
 
 #execute the query
@@ -126,11 +111,12 @@ s_time_dbt3=`$GTIME`
 echo "`date +'%Y-%m-%d %H:%M:%S'` start load test" 
 #get the start time
 s_time=`$GTIME`
-$sapdb_script_path/build_db.sh
+$builddb_path/build_db.pl -f build_db.conf
 e_time=`$GTIME`
 echo "`date +'%Y-%m-%d %H:%M:%S'` load test end" 
 let "diff_time_load=$e_time-$s_time"
 echo "elapsed time for load test $diff_time_load" 
+
 
 #calculate count 
 let "count=($duration-$diff_time_load)/$interval"
@@ -157,7 +143,11 @@ echo "`date`: dbt3 test end"
 let "diff_time_dbt3=$e_time_dbt3-$s_time_dbt3"
 echo "elapsed time for dbt3 test $diff_time_dbt3" 
 
-wait
+ps -ef | grep -v grep | grep sar | awk '{print $2}' | xargs kill -9
+ps -ef | grep -v grep | grep iostat | awk '{print $2}' | xargs kill -9
+ps -ef | grep -v grep | grep vmstat | awk '{print $2}' | xargs kill -9
+pgrep db_stats.sh | xargs kill -9
+
 #get meminfo
 cat /proc/meminfo > $output_dir/meminfo1.out
 
@@ -165,7 +155,7 @@ cat /proc/meminfo > $output_dir/meminfo1.out
 dbmcli -d $SID -u dbm,dbm -uSQL dbt,dbt "sql_execute select task_name, s_time, e_time, timediff(e_time,s_time) from time_statistics" 2>&1 > $output_dir/q_time.out
 
 #calculate composite power
-$dbdriver_script_path/get_composite.pl -p 1 -s 1 -n $num_stream -o $output_dir/calc_composite.out 
+$dbdriver_script_path/get_composite.pl -p 1 -s 1 -n $num_stream -o $output_dir/calc_composite.out
 
 #copy dbt3.out
 mv $datacollect_sapdb_path/dbt3.out $output_dir/dbt3.out
