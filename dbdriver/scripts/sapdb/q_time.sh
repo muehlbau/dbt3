85b3eae3d9d820b20754da257b7270e34db04de0
jztpcw <devnull@localhost>
2003-07-17 20:36:45 +0000

    [project @ 1153] changed dbt to

--- dbdriver/scripts/sapdb/q_time.sh
+++ dbdriver/scripts/sapdb/q_time.sh
@@ -8,4 +8,4 @@
 #
 # Author: Jenny Zhang
 #
-dbmcli -d DBT3 -u dbm,dbm -uSQL dbt,dbt "sql_execute select task_name, s_time, e_time, timediff(e_time,s_time) from time_statistics"
+dbmcli -d DBT3 -u dbm,dbm -uSQL $DBUSER,$DBUSER "sql_execute select task_name, s_time, e_time, timediff(e_time,s_time) from time_statistics"
