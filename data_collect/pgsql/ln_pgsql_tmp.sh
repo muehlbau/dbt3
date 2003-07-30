557689e1dd7b73f2b464cd2a67f72c44189550f6
jztpcw <devnull@localhost>
2003-07-30 23:39:20 +0000

    [project @ 1175] create symbolic link for pgsql_tmp

---
+++ data_collect/pgsql/ln_pgsql_tmp.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -x
+
+# assumes the user created database files is the last one
+# will not work if there are more than 1 user created database
+for i in $PGDATA/base/*
+do
+#	if [$i > 17000]; then
+	dbdir=$i
+done
+
+rm -rf $dbdir/pgsql_tmp
+ln -s /dbt3_tmp/dbt3 $dbdir/pgsql_tmp
