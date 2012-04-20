2a27e0d771d4ac13eb0236629340ceb64726b201
Satoshi Nagayasu <satoshi.nagayasu@gmail.com>
2012-04-20 11:40:34 +0900

    - Replaced setup scripts ported from PostgreSQL ones.

--- scripts/mysql/backup_db.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-_test=`pg_dump $SID --file=$DBT3_BACKUP | grep OK`
-if [ "$_test" != "" ]; then
-        echo "backup failed: $_o"
-        exit 1
-fi
--- scripts/mysql/define_medium.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "define backup medium"
-export DBT3_BACKUP=/tmp/$SID.db
